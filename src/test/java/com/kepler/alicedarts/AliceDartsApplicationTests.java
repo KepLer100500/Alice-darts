8a7ab4dc21be56a7c994c251e41e98ca697811a9
KepLer <kepler007@mail.ru>
2023-07-01 12:14:25 +0400

    Init commit. Create project.

---
+++ src/test/java/com/kepler/alicedarts/AliceDartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kepler.alicedarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliceDartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
